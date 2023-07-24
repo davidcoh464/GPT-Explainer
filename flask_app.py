500ee87d6230d1a0eba7050a1aff1bb35fe624bb
David Cohen <deiv464@gmail.com>
2023-07-24 23:07:35 +0300

    Separate db from flask

--- flask_app.py
+++ flask_app.py
@@ -1,42 +1,17 @@
 from flask import Flask, Response, request, jsonify, render_template, redirect, flash, url_for
 from dotenv import load_dotenv
-from flask_util import set_path, generate_uid, load_output, save_to_json
-from flask_util import UPLOADS_FOLDER, OUTPUTS_FOLDER, os
+from flask_util import set_path, load_output, save_to_json
+from flask_util import UPLOADS_FOLDER, os
 import json
 import threading
-from presentation_parser import PresentationParser
-from slide_handler import SlideHandler
-from output_manage import OutputManage
-import asyncio
-import sys
 from datetime import datetime
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Enum, ForeignKey, String, DateTime
-from sqlalchemy.orm import Mapped, mapped_column, relationship
-from typing import List, Optional
+from flask_explainer import explainer_system, setup_explainer
+from db_model import Session, User, Upload, create_all
+
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
-db = SQLAlchemy(app)
-
-TIME_TO_SLEEP = 10
-WINDOWS_PLATFORM = 'win'
-
-
-class User(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(128), unique=True, nullable=False)
-    uploads: Mapped[List["Upload"]] = relationship("Upload", backref='user', lazy=True, cascade='all, delete-orphan')
-
-
-class Upload(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    uid: Mapped[str] = mapped_column(String(36), default=generate_uid, nullable=False, unique=True)
-    filename: Mapped[str] = mapped_column(String(128), nullable=False)
-    upload_time: Mapped[DateTime] = mapped_column(DateTime, default=datetime.utcnow)
-    finish_time: Mapped[Optional[DateTime]] = mapped_column(DateTime)
-    status: Mapped[str] = mapped_column(Enum("pending", "done", name="upload_status"), default="pending")
-    user_id: Mapped[Optional[int]] = mapped_column(ForeignKey('user.id'))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/db.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
 def setup_app():
@@ -48,89 +23,34 @@ def setup_app():
     app.secret_key = os.getenv("SECRET_KEY")
     app.config['MAX_CONTENT_LENGTH'] = 16 * 1000 * 1000
     app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
-    with app.app_context():
-        db.create_all()
+    create_all()
     set_path()
     setup_explainer()
 
 
-def setup_explainer():
-    """
-    Sets up the explainer system.
-
-    This function should be called before starting the explainer system
-    to set up any necessary configurations or environment settings.
-    """
-
-    # Set asyncio platform
-    if sys.platform.startswith(WINDOWS_PLATFORM):
-        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-
-
-def process_file(filename: str):
-    """
-    Processes the uploaded file by extracting text from presentation slides,
-    handling the slides asynchronously, and saving the responses as JSON.
-
-    Args:
-        filename (str): The filename of the uploaded file to be processed.
-    """
-
-    upload_path = f"{UPLOADS_FOLDER}/{filename}"
-    slides = PresentationParser.extract_text(upload_path)
-    responses = asyncio.run(SlideHandler.response_handler(slides))
-    output_path = f"{OUTPUTS_FOLDER}/{filename}"
-    OutputManage.save_to_json(responses, output_path)
-
-
-def explainer_system(stop_event: threading.Event):
-    """
-    Implements the background explainer system that continuously processes
-    files in the uploads folder that are not yet processed.
-
-    This function runs in an infinite loop until the stop event is set.
-    It processes the files using the process_file() function and sleeps for
-    TIME_TO_SLEEP seconds between iterations.
-
-    Args:
-        stop_event (threading.Event): The event to signal the system to stop.
-    """
-    while not stop_event.is_set():
-        with app.app_context():
-            upload_files = Upload.query.filter_by(status="pending").all()
-            for upload_file in upload_files:
-                if not stop_event.is_set():
-                    try:
-                        _, file_type = os.path.splitext(upload_file.filename)
-                        process_file(f"{upload_file.uid}{file_type}")
-                        upload_file.finish_time = datetime.now()
-                        upload_file.status = "done"
-                        db.session.commit()
-                    except KeyError:
-                        continue
-        stop_event.wait(timeout=TIME_TO_SLEEP)
-
-
 def save_upload(file):
-    anonymous_upload = Upload(filename=file.filename)
-    db.session.add(anonymous_upload)
-    db.session.commit()
-    _, file_type = os.path.splitext(file.filename)
-    file.save(os.path.join(UPLOADS_FOLDER, f"{anonymous_upload.uid}{file_type}"))
-    return anonymous_upload.uid
+    with Session() as session:
+        anonymous_upload = Upload(filename=file.filename, upload_time=datetime.now())
+        session.add(anonymous_upload)
+        session.commit()
+        _, file_type = os.path.splitext(file.filename)
+        file.save(os.path.join(UPLOADS_FOLDER, f"{anonymous_upload.uid}{file_type}"))
+        return anonymous_upload.uid
 
 
 def save_upload_with_user(file, email: str):
-    user = User.query.filter_by(email=email).first()
-    if not user:
-        user = User(email=email)
-        db.session.add(user)
-    user_upload = Upload(filename=file.filename, user=user)
-    db.session.add(user_upload)
-    db.session.commit()
-    _, file_type = os.path.splitext(file.filename)
-    file.save(os.path.join(UPLOADS_FOLDER, f"{user_upload.uid}{file_type}"))
-    return user_upload.uid
+    with Session() as session:
+        user = session.query(User).filter_by(email=email).first()
+        if not user:
+            user = User(email=email)
+            session.add(user)
+            session.commit()
+        user_upload = Upload(filename=file.filename, upload_time=datetime.now(), user=user)
+        session.add(user_upload)
+        session.commit()
+        _, file_type = os.path.splitext(file.filename)
+        file.save(os.path.join(UPLOADS_FOLDER, f"{user_upload.uid}{file_type}"))
+        return user_upload.uid
 
 
 @app.route('/')
@@ -160,17 +80,16 @@ def upload():
         if 'file' not in request.files:
             flash('No file part')
             return redirect(request.url)
-        file = request.files['file']
+        file = request.files.get('file')
         if file.filename == '':
             flash('No selected file')
             return redirect(request.url)
-        email = request.form["email"]
+        email = request.form.get('email')
         if email:
             uid = save_upload_with_user(file, email)
         else:
             uid = save_upload(file)
-        response = {'uid': uid}
-        return jsonify(response)
+        return jsonify({'uid': uid}), 200
     return render_template("upload.html")
 
 
@@ -189,14 +108,15 @@ def status(uid):
     Returns:
         Response: A JSON response with the processing status and information.
     """
-    file_data = Upload.query.filter_by(uid=uid).first()
-    if file_data:
-        if file_data.status == "done":
-            output = load_output(f"{uid}.json")
-            status_info = save_to_json(uid, file_data.status, file_data.filename, file_data.finish_time, output)
-        else:
-            status_info = save_to_json(uid, file_data.status, file_data.filename, file_data.finish_time)
-        return Response(json.dumps(status_info), mimetype='application/json')
+    with Session() as session:
+        file_data = session.query(Upload).filter_by(uid=uid).first()
+        if file_data:
+            if file_data.status == "done":
+                output = load_output(f"{uid}")
+                status_info = save_to_json(uid, file_data.status, file_data.filename, file_data.finish_time, output)
+            else:
+                status_info = save_to_json(uid, file_data.status, file_data.filename, file_data.finish_time)
+            return Response(json.dumps(status_info), mimetype='application/json')
     return jsonify({'status': 'not found'}), 404
 
 
@@ -223,16 +143,17 @@ def search():
         if uid:
             return redirect(url_for('status', uid=uid))
         elif email and filename:  # Only search if both email and filename are provided
-            user = User.query.filter_by(email=email).first()
-            if user:
-                latest_upload = Upload.query.filter_by(user=user, filename=filename).order_by(
-                    Upload.upload_time.desc()).first()
-                if latest_upload:
-                    return redirect(url_for('status', uid=latest_upload.uid))
+            with Session() as session:
+                user = session.query(User).filter_by(email=email).first()
+                if user:
+                    latest_upload = session.query(Upload).filter_by(user=user, filename=filename).order_by(
+                        Upload.upload_time.desc()).first()
+                    if latest_upload:
+                        return redirect(url_for('status', uid=latest_upload.uid))
+                    else:
+                        flash("Filename not found")
                 else:
-                    flash("Filename not found")
-            else:
-                flash(f"Email: {email} does not exist")
+                    flash(f"Email: {email} does not exist")
         else:
             flash("Please enter a UID, or provide both email and filename")
         return redirect(request.url)
@@ -253,8 +174,6 @@ def main():
     app.run()
     stop_event.set()
     t1.join()
-    with app.app_context():
-        db.session.close()
 
 
 if __name__ == '__main__':
---
+++ flask_explainer.py
@@ -0,0 +1,70 @@
+from presentation_parser import PresentationParser
+from slide_handler import SlideHandler
+from output_manage import OutputManage
+from datetime import datetime
+from flask_util import UPLOADS_FOLDER, OUTPUTS_FOLDER
+from db_model import Session, Upload
+import threading
+import asyncio
+import os
+import sys
+
+TIME_TO_SLEEP = 10
+WINDOWS_PLATFORM = 'win'
+
+
+def setup_explainer():
+    """
+    Sets up the explainer system.
+
+    This function should be called before starting the explainer system
+    to set up any necessary configurations or environment settings.
+    """
+
+    # Set asyncio platform
+    if sys.platform.startswith(WINDOWS_PLATFORM):
+        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
+
+def process_file(filename: str):
+    """
+    Processes the uploaded file by extracting text from presentation slides,
+    handling the slides asynchronously, and saving the responses as JSON.
+
+    Args:
+        filename (str): The filename of the uploaded file to be processed.
+    """
+
+    upload_path = f"{UPLOADS_FOLDER}/{filename}"
+    slides = PresentationParser.extract_text(upload_path)
+    responses = asyncio.run(SlideHandler.response_handler(slides))
+    output_path = f"{OUTPUTS_FOLDER}/{filename}"
+    OutputManage.save_to_json(responses, output_path)
+
+
+def explainer_system(stop_event: threading.Event):
+    """
+    Implements the background explainer system that continuously processes
+    files in the uploads folder that are not yet processed.
+
+    This function runs in an infinite loop until the stop event is set.
+    It processes the files using the process_file() function and sleeps for
+    TIME_TO_SLEEP seconds between iterations.
+
+    Args:
+        stop_event (threading.Event): The event to signal the system to stop.
+    """
+    while not stop_event.is_set():
+        with Session() as session:
+            upload_files = session.query(Upload).filter_by(status="pending").all()
+            for upload_file in upload_files:
+                if not stop_event.is_set():
+                    try:
+                        _, file_type = os.path.splitext(upload_file.filename)
+                        process_file(f"{upload_file.uid}{file_type}")
+                        upload_file.finish_time = datetime.now()
+                        upload_file.status = "done"
+                        session.commit()
+                    except KeyError:
+                        continue
+        stop_event.wait(timeout=TIME_TO_SLEEP)
