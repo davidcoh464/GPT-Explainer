8619e1238b929310b277541bcb0edb179cf2a39c
David Cohen <deiv464@gmail.com>
2023-12-12 13:25:07 +0200

    Updating the tests for the changes

--- tests/flask_test.py
+++ tests/flask_test.py
@@ -1,7 +1,7 @@
 import pytest
-from flask_app import app
+from flask_app import app, setup_app
+from tests.test_util import clear_resource
 import json
-import os
 
 # Define the path to your file
 FILE_PATH = 'can you.pptx'
@@ -13,6 +13,7 @@ def client():
     Fixture to provide a test client for the Flask app.
     It sets the app's testing configuration and yields the test client.
     """
+    setup_app()
     app.config['TESTING'] = True
     with app.test_client() as client:
         yield client
@@ -33,9 +34,12 @@ def test_upload(client):
     Test case for the upload route ("/upload").
     It sends a POST request with a file to the upload route and asserts the response.
     """
-    response = client.post('/upload', data={'file': (open(f'{FILE_PATH}', 'rb'), f'{FILE_PATH}')})
+    with open(FILE_PATH, 'rb') as file:
+        response = client.post('/upload', data={'file': (file, FILE_PATH)})
     assert response.status_code == 200
     assert b"uid" in response.data
+    data = json.loads(response.data)
+    clear_resource(data['uid'])
 
 
 def test_status(client):
@@ -54,7 +58,7 @@ def test_upload_status(client):
     It uploads a file and then sends a GET request to the status route using the uploaded UID.
     It asserts the response to check if the status is "pending".
     """
-    response = client.post('/upload', data={'file': (open(f'{FILE_PATH}', 'rb'), 'can you.pptx')})
+    response = client.post('/upload', data={'file': (open(f'{FILE_PATH}', 'rb'), f'{FILE_PATH}')})
     assert response.status_code == 200
     data = json.loads(response.data)
     uid = data.get('uid')
@@ -63,3 +67,4 @@ def test_upload_status(client):
     data = json.loads(response.data)
     status = data["status"]
     assert status == "pending"
+    clear_resource(uid)
